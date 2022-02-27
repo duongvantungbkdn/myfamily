c4e9e7f46427b54126aac784637184a4c0cfa9e5
Duong Tung <duongvantungbkdn@gmail.com>
2022-02-27 21:17:12 +0700

    commit 1

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'          
         ? 'http://localhost:5000'
-        : 'https://myfamilytung.herokuapp.com'
+        : ' https://myfamilytung2.herokuapp.com'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'my_family'
 
